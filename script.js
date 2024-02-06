8bfb05233e61f24e90476eb00fcfffc3c0f74c29
laetitia piat <laeptitchat29@gmail.com>
2024-02-06 18:06:50 +0100

    modif code js

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-console.log("connecté!");
-
 //Je selectionne et je stock la div conteneur-photo
-const postConteneur = document.querySelector("conteneur-photo");
-console.log(postConteneur);
+let postConteneur = document.querySelector(".conteneur-photo");
 
 //Je créé un tableau avec mes objets
 const posts = [
   {
     titre: "Tom",
-    image: "/photo/Tom1.jpg",
+    image: "photo/Tom1.jpg",
   },
   {
     titre: "Tom",
-    image: "/photo/Tom2.jpg",
+    image: "photo/Tom2.jpg",
   },
   {
     titre: "Tom",
-    image: "/photo/Tom3.jpg",
+    image: "photo/Tom3.jpg",
   },
   {
     titre: "Brad",
-    image: "/photo/Brad1.jpg",
+    image: "photo/Brad1.jpg",
   },
   {
     titre: "Brad",
-    image: "/photo/Brad2.jpg",
+    image: "photo/Brad2.jpg",
   },
 ];
-console.log(posts);
+
+//Boucler pour faire apparaitre chaque objet du tableau sur notre blog
+posts.forEach((item) => {
+  //creation d'une div
+  const article = `
+    <div class = "post">
+      <div class = "post-titre">${item.titre}</div>
+      <img src = ${item.image} "post-image">
+    </div>
+  `;
+  postConteneur.innerHTML += article;
+});
