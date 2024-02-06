5361b39f5d176e43786fbbb73279c4980e3145dc
laetitia piat <laeptitchat29@gmail.com>
2024-02-06 16:03:48 +0100

    ajout code js

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("connecté!");
+
+//Je selectionne et je stock la div conteneur-photo
+const postConteneur = document.querySelector("conteneur-photo");
+console.log(postConteneur);
+
+//Je créé un tableau avec mes objets
+const posts = [
+  {
+    titre: "Tom",
+    image: "/photo/Tom1.jpg",
+  },
+  {
+    titre: "Tom",
+    image: "/photo/Tom2.jpg",
+  },
+  {
+    titre: "Tom",
+    image: "/photo/Tom3.jpg",
+  },
+  {
+    titre: "Brad",
+    image: "/photo/Brad1.jpg",
+  },
+  {
+    titre: "Brad",
+    image: "/photo/Brad2.jpg",
+  },
+];
+console.log(posts);
