e3987dd0df891ed3333e9fe7a18d1b5b4af73bbb
laetitia piat <laeptitchat29@gmail.com>
2024-02-27 16:20:33 +0100

    ajout page auth

--- auth.js
+++ auth.js
@@ -1,36 +1,34 @@
 const authentification = document.querySelector(".wrapper");
 
-document.querySelector(".input-submit").addEventListener("click", function() {
- console.log("cliqué!"),
+const user = document.querySelector("#user");
 
-  authentification.style.opacity="0";
+const pass = document.querySelector("#pass");
 
-
- const users = [
-    {
-        mail : "parent-tom@gmail.com",
-        password : "parent-tom",
-    },
-    {
-        mail : "parent-gaga@gmail.com",
-        password : "parent-gaga",
-    },
-    {
-        mail : "parent-brad@gmail.com",
-        password : "parent-brad",
-    },
-    {
-        mail : "parent-jennifer@gmail.com",
-        password : "parent-jennifer",
-    },
-    ]
-
-})
+const btnValid =document.querySelector(".input-submit");
 
 
+const users = [
+{
+    mail : "parent-tom@gmail.com",
+    password : "parent-tom",
+},
+{
+    mail : "parent-gaga@gmail.com",
+    password : "parent-gaga",
+},
+{
+    mail : "parent-brad@gmail.com",
+    password : "parent-brad",
+},
+{
+    mail : "parent-jennifer@gmail.com",
+    password : "parent-jennifer",
+},
+]
 
-
-
-    //if user == users.mail && pass == users.password
-   // authentification.style.opacity="0";
+btnValid.addEventListener('click', function(){
+   // if user == users.mail && pass == users.password
+   console.log("cliqué!")
+    authentification.style.display = "none"
     
+})
