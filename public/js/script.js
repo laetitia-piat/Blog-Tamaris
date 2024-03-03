894a4d9f4568b99d3f78c8076b34d0a7731f620d
laetitia piat <laeptitchat29@gmail.com>
2024-03-03 15:14:30 +0100

    modif chemin

--- public/js/script.js
+++ public/js/script.js
@@ -5,43 +5,43 @@ let postConteneur = document.querySelector(".conteneur-photo");
 const posts = [
   {
     titre: "Tom",
-    image: "photo/Tom1.jpg"
+    image: "public/photo/Tom1.jpg"
   },
   {
     titre: "Jennifer",
-    image: "photo/Jen1.jpg",
+    image: "public/photo/Jen1.jpg",
   },
   {
     titre: "Brad",
-    image: "photo/Brad2.jpg",
+    image: "public/photo/Brad2.jpg",
   },
   {
     titre: "Lady Gaga",
-    image: "photo/Lady1.jpg",
+    image: "public/photo/Lady1.jpg",
   },
   {
     titre: "Jennifer",
-    image: "photo/Jen2.jpg",
+    image: "public/photo/Jen2.jpg",
   },
   {
     titre: "Tom",
-    image: "photo/Tom2.jpg",
+    image: "public/photo/Tom2.jpg",
   },
   {
     titre: "Lady Gaga",
-    image: "photo/Lady3.jpg",
+    image: "public/photo/Lady3.jpg",
   },
   {
     titre: "Tom",
-    image: "photo/Tom3.jpg",
+    image: "public/photo/Tom3.jpg",
   },
   {
     titre: "Brad",
-    image: "photo/Brad1.jpg",
+    image: "public/photo/Brad1.jpg",
   },
   {
     titre: "Lady Gaga",
-    image: "photo/Lady2.jpg",
+    image: "public/photo/Lady2.jpg",
   },
 ];
 
